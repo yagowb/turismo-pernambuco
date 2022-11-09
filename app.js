ded6bc7683b3fbcf97f5a24faaa8cc5de4f082cb
yago rodrigues <yagodev3@gmail.com>
2022-11-09 20:00:22 -0300

    Adição de nova rota Destinos & Ajustes de CSS

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const path = require('path');
 
 
 
+app.use("/images", express.static(path.join(__dirname, '/images')));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
-  app.get("/quem-somos.html", (req, res) => {
+app.get("/quem-somos.html", (req, res) => {
     res.sendFile(__dirname + "/quem-somos.html");
   });
   
+app.get("/destinos.html", (req, res) => {
+    res.sendFile(__dirname + "/destinos.html");
+  });
 
 //Inicialização do Servidor//
 app.listen(process.env.PORT ?? 4000);
